692fb2e6937ca3c56edc74fd3179030174df215f
rajeeva <rajeevabp92@gmail.com>
2023-07-02 16:52:42 +0530

    added the js

---
+++ mobile-cart/js/app.js
@@ -0,0 +1,12 @@
+function app() {
+    this.select = document.getElementsByTagName('select');
+    this.listing = document.getElementById('listing');
+    this.nameInput = document.getElementsByClassName('text-input');
+    this.checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+    this.checkboxLabel = document.querySelector('.price-filter label');
+
+
+    // innerHTML, innerText, textContent
+}
+
+app();
